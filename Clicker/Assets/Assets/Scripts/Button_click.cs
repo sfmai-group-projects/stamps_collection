0d9f0c0a9ba25a8e82147a7453cbc73de33c7edd
Kirill <sayfutdinov-8928@mail.ru>
2023-11-01 10:29:30 +0300

    Создал скрипт
    Создал скрипт для подсчета кликов и отображения в текстовое поле.

---
+++ Clicker/Assets/Assets/Scripts/Button_click.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button_click : MonoBehaviour
+{
+    [SerializeField] int score;
+    public Text scoreText;
+
+    public void click()
+    {
+        score++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = score.ToString();
+    }
+}
